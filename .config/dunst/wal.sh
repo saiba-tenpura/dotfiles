6d11840f31b45dead3f7b28528af327822bbef9d
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2022-12-02 23:45:38 +0100

    Add dunst config via pywal user template.

---
+++ .config/dunst/wal.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Symlink dunst config
+ln -sf ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
+
+# Restart dunst with the new color scheme
+pkill dunst
+dunst &
+
---
+++ .config/wal/done.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ -e ~/.cache/dunst/wal.sh ]; then
+    ~/.config/dunst/wal.sh
+fi
+
