b39904902c1ecff7c39bd23e653dc24b57bb61f4
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2022-12-12 13:32:48 +0100

    Add setup, help, run functions and move configuration to backup script.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,84 @@
 #!/usr/bin/env bash
 
 export RESTIC_REPOSITORY="/storage/restic-repo"
-export RESTIC_PASSWORD_FILE="$HOME/.config/backup/.restic-password"
+export RESTIC_PASSWORD_FILE="/root/.config/.restic"
+FILES=(
+    /home/tenpura/.mozilla/firefox/*.default*
+    /home/tenpura/.ssh
+    /home/tenpura/.thunderbird/*.default*
+    /home/tenpura/.ts3client/{chats,identity.ini,settings.db,urls.db}
+    /home/tenpura/Documents
+    /home/tenpura/Downloads
+    /storage/virtual-machines/ubuntu-22.04
+)
 
-restic backup --files-from ~/.config/backup/include.txt
-restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 --prune
-restic check | tee -a /var/log/restic.log
+usage() {
+    cat <<-EOF
+	Usage: $(basename "$0") [options]
+
+	Options:
+	-h, --help      Show this help message.
+	-r, --run       Execute backup of target files, prune old backups afterwards and check backup integrity.
+	-s, --setup     Setup password file, init repository and create cron entry.
+	EOF
+
+    exit 1
+}
+
+error() {
+    printf '\e[93mERROR:\e[m %s\n' "${1}"
+    exit 2
+}
+
+check_root() {
+    [ "$EUID" -ne 0 ] && error "Script must be executed as root!"
+}
+
+run() {
+    restic backup -vv --tag $(hostname -s) $@
+    restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 24 --prune
+    restic check | tee -a /var/log/restic.log
+
+    exit 0
+}
+
+setup() {
+    [ -f "$RESTIC_PASSWORD_FILE" ] && error "Password file already exists. Aborting setup!"
+
+    printf 'Password:\n'
+    read -s password
+    printf 'Confirm Password:\n'
+    read -s password_confirmation
+    [[ "${password}" != "${password_confirmation}" ]] && error "Passwords do not match!"
+
+    echo "$password" > "$RESTIC_PASSWORD_FILE"
+    printf 'Init new restic repository!\n'
+    mkdir -p "$RESTIC_REPOSITORY"
+    restic init
+
+    printf 'Setup crontab!\n'
+    SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+    SCRIPT_NAME="$(basename $0)"
+    echo "30 9 * * * root ${SCRIPT_DIR}/${SCRIPT_NAME} -r" > /etc/cron.d/restic
+
+    exit 0
+}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            usage
+            ;;
+        -s|--setup)
+            check_root
+            setup
+            ;;
+        -r|--run)
+            check_root
+            run ${FILES[@]}
+            ;;
+        *)
+            error "Unkown option $1 was given. See -h|--help for available options."
+            ;;
+   esac
+done
