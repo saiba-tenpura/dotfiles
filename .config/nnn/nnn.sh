39aa769dc99beabb30a3da7aaedbff3fbc62ddb5
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-02-03 21:45:07 +0100

    Add projects folder to bookmarks.

--- .config/nnn/nnn.sh
+++ .config/nnn/nnn.sh
@@ -5,7 +5,7 @@ if [ -f /usr/share/nnn/quitcd/quitcd.bash_zsh ]; then
     . /usr/share/nnn/quitcd/quitcd.bash_zsh
 fi
 
-export NNN_BMS='d:~/Documents;D:~/Downloads'
+export NNN_BMS='d:~/Documents;D:~/Downloads;p:~/Documents/Saiba/Projects'
 export NNN_FIFO='/tmp/nnn.fifo'
 export NNN_OPTS='dH'
 export NNN_PLUG='p:preview-tui;v:imgview'
