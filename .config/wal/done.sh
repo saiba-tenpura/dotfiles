b9403d2c975c1234d75db250170bb74ca1551825
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-05-24 23:37:27 +0200

    Add pywal color handling for hypr colors.

--- .config/wal/done.sh
+++ .config/wal/done.sh
@@ -4,6 +4,10 @@ if [ -e ~/.config/dunst/wal.sh ]; then
     ~/.config/dunst/wal.sh
 fi
 
+if [ -e ~/.config/hypr/wal.sh ]; then
+    ~/.config/hypr/wal.sh
+fi
+
 if [ -e ~/.config/zathura/wal.sh ]; then
     ~/.config/zathura/wal.sh
 fi
--- scripts/wal.sh
+++ scripts/wal.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# Gen pywal & update polybar
-~/.config/polybar/docky/scripts/pywal.sh $1
+if [[ "$XDG_SESSION_TYPE" == 'wayland' ]]; then
+    # Gen pywal
+    wal -i "$1" -q -t
+else
+    # Gen pywal & update polybar
+    ~/.config/polybar/docky/scripts/pywal.sh $1
+fi
 
 # Run post script
 ~/.config/wal/done.sh
