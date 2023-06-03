87e5d9f445db1738d4f5f027108f3501e2b117a4
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-06-03 15:24:12 +0200

    Fix swayidle script.

--- .config/sway/idle.sh
+++ .config/sway/idle.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash 
 
 swayidle -w \
-    timeout 300 '~/.config/swaylock/lock.sh --grace 5 --fade-in 10' \
-    timeout 360 'swaymsg "output * dpms off"' \
-        resume 'swaymsg "output * dpms on"' \
-    before-sleep "~/.config/swaylock/lock.sh --fade-in 0" &
+    timeout 300 '~/.config/sway/lock.sh --grace 5 --fade-in 10' \
+    timeout 360 'hyprctl dispatch dpms off' \
+        resume 'hyprctl dispatch dpms on' \
+    before-sleep "~/.config/sway/lock.sh --fade-in 0"
 
