fc5c5b8c5fdbc17aed6cd4f1abda95a639386396
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-06-12 18:14:26 +0200

    Setup Neovim completion key bindings and icons.

--- .config/nvim/lua/saiba/lsp.lua
+++ .config/nvim/lua/saiba/lsp.lua
@@ -16,3 +16,58 @@ require('lspconfig').lua_ls.setup({
 })
 
 lsp.setup()
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local kind_icons = {
+  Text = "󰦨",
+  Method = "",
+  Function = "󰊕",
+  Constructor = "󱁤",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "󰅩",
+  Property = "",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
+cmp.setup({
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+      vim_item.menu = ({
+        buffer = '[Buffer]',
+        nvim_lsp = '[LSP]',
+        luasnip = '[LuaSnip]',
+        nvim_lua = '[Lua]',
+        latex_symbols = '[LaTeX]',
+      })[entry.source.name]
+      return vim_item
+    end
+  },
+  mapping = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  })
+})
