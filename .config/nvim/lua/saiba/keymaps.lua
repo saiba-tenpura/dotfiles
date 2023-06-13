cbc4bbf0dcc8625d04d86c97ab95433004c462b6
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-06-13 16:28:51 +0200

    Use Neovim function for setting keymaps instead of API.

--- .config/nvim/lua/saiba/keymaps.lua
+++ .config/nvim/lua/saiba/keymaps.lua
@@ -1,43 +1,43 @@
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- Remap leader to space
-keymap("", "<Space>", "<Nop>", opts)
+keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal Mode --
 -- Window Navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrow keys
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap.set("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap.set("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Move line up/down with alt + j/k
-keymap("n", "<A-j>", ":m +1<CR>", opts)
-keymap("n", "<A-k>", ":m -2<CR>", opts)
+keymap.set("n", "<A-j>", ":m +1<CR>", opts)
+keymap.set("n", "<A-k>", ":m -2<CR>", opts)
 
 -- Insert Mode --
 -- Move line up/down with alt + j/k
-keymap("i", "<A-j>", "<ESC>:m +1<CR>gi", opts)
-keymap("i", "<A-k>", "<ESC>:m -2<CR>gi", opts)
+keymap.set("i", "<A-j>", "<ESC>:m +1<CR>gi", opts)
+keymap.set("i", "<A-k>", "<ESC>:m -2<CR>gi", opts)
 
 -- Visual Mode --
 -- Move lines up/down with alt + j/k
-keymap("v", "<A-j>", ":m '>+1<CR>gvgv", opts)
-keymap("v", "<A-k>", ":m '<-2<CR>gvgv", opts)
+keymap.set("v", "<A-j>", ":m '>+1<CR>gvgv", opts)
+keymap.set("v", "<A-k>", ":m '<-2<CR>gvgv", opts)
 
 -- Persist register on paste
-keymap("v", "p", '"_dP', opts)
+keymap.set("v", "p", '"_dP', opts)
 
 -- Plugins --
 -- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>gf", ":Telescope git_files<CR>", opts)
-keymap("n", "<leader>lg", ":Telescope live_grep<CR>", opts)
+keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap.set("n", "<leader>gf", ":Telescope git_files<CR>", opts)
+keymap.set("n", "<leader>lg", ":Telescope live_grep<CR>", opts)
