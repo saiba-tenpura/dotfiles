5e2ed1dfeff6ab41b4244c804baec35fa5049db3
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-06-28 21:22:06 +0200

    Swap resize direction for Neovim shortcut.

--- .config/nvim/lua/saiba/keymaps.lua
+++ .config/nvim/lua/saiba/keymaps.lua
@@ -18,8 +18,8 @@ keymap("n", "<S-l>", ':bnext<CR>', opts)
 keymap("n", "<S-h>", ':bprevious<CR>', opts)
 
 -- Resize with arrow keys
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
