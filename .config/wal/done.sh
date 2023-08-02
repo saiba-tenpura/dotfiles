b9c330b45a066f34054060bacfd2138c22e3a436
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2023-08-02 23:12:16 +0200

    Prevent wal done script from failing with X11 display server.

--- .config/wal/done.sh
+++ .config/wal/done.sh
@@ -4,19 +4,20 @@ if [ -e ~/.config/dunst/wal.sh ]; then
     ~/.config/dunst/wal.sh
 fi
 
-if [ -e ~/.config/hypr/wal.sh ]; then
-    ~/.config/hypr/wal.sh
-fi
-
-if [ -e ~/.config/waybar/wal.sh ]; then
-    ~/.config/waybar/wal.sh
-fi
-
-if [ -e ~/.config/wlogout/wal.sh ]; then
-    ~/.config/wlogout/wal.sh
-fi
-
 if [ -e ~/.config/zathura/wal.sh ]; then
     ~/.config/zathura/wal.sh
 fi
 
+if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
+    if [ -e ~/.config/hypr/wal.sh ]; then
+        ~/.config/hypr/wal.sh
+    fi
+
+    if [ -e ~/.config/waybar/wal.sh ]; then
+        ~/.config/waybar/wal.sh
+    fi
+
+    if [ -e ~/.config/wlogout/wal.sh ]; then
+        ~/.config/wlogout/wal.sh
+    fi
+fi
