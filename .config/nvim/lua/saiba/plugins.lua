881b879748abbf519d3f395160cfc033c81500ff
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-05-17 21:12:14 +0200

    Switch from packer.nvim to lazy.nvim for Neovim plugin manager.

--- .config/nvim/lua/saiba/plugins.lua
+++ .config/nvim/lua/saiba/plugins.lua
@@ -1,101 +1,59 @@
--- Install packer
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+-- Install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Reload Neovim on plugins.lua changes
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-  augroup end
-]])
-
-local packer_bootstrap = ensure_packer()
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-
+require('lazy').setup({
   -- Autopairs
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
+  'windwp/nvim-autopairs',
+  'windwp/nvim-ts-autotag',
 
   -- Bufferline
-  use {
-    'akinsho/bufferline.nvim', tag = '*',
-    requires = 'nvim-tree/nvim-web-devicons'
-  }
+  {'akinsho/bufferline.nvim', version = '*', dependencies = {'nvim-tree/nvim-web-devicons'}},
 
   -- Colorscheme
-  use 'rktjmp/lush.nvim'
-  use { 'saiba-tenpura/pywal-theme.nvim', as = 'pywal-theme' }
+  'rktjmp/lush.nvim',
+  {'saiba-tenpura/pywal-theme.nvim', as = 'pywal-theme'},
 
   -- Colorizer
-  use 'norcalli/nvim-colorizer.lua'
+  'norcalli/nvim-colorizer.lua',
 
-  -- Extendable fuzzy finder
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.3',
-    requires = {{'nvim-lua/plenary.nvim'}}
-  }
+  -- Extendable Fuzzy Finder
+  {'nvim-telescope/telescope.nvim', tag = '0.1.6', dependencies = {'nvim-lua/plenary.nvim'}},
 
   -- Git
-  use 'lewis6991/gitsigns.nvim'
+  'lewis6991/gitsigns.nvim',
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+  -- LSP Highlighting
+  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
 
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {{'nvim-tree/nvim-web-devicons', opt = true}}
-  }
+  -- Explorer
+  {'nvim-tree/nvim-tree.lua', version = '*', lazy = false, dependencies = {'nvim-tree/nvim-web-devicons'}},
 
   -- Icons
-  use 'nvim-tree/nvim-web-devicons'
-  use 'onsails/lspkind-nvim'
+  'nvim-tree/nvim-web-devicons',
+  'onsails/lspkind-nvim',
 
   -- LaTeX
-  use 'lervag/vimtex'
+  {'lervag/vimtex', lazy = false},
 
   -- LSP Support
-  use {
-    'VonHeikemen/lsp-zero.nvim', branch = 'v2.x',
-    requires = {
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
-    }
-  }
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
 
   -- Statusline
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-  }
-
-  -- Miscellanous
-  use 'mbbill/undotree'
-
-  -- Automatically setup configuration after cloning packer
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  {'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}}
+})
