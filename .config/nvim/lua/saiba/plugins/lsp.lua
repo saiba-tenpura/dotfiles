9e0f79ddd85e1e0344584b63ddac78ea2b0f3aa1
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2024-06-11 22:51:34 +0200

    Fix LSP setup and incl. lazy loading.

--- .config/nvim/lua/saiba/plugins/lsp.lua
+++ .config/nvim/lua/saiba/plugins/lsp.lua
@@ -1,51 +1,79 @@
 return {
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  config = function()
-    local lsp_zero = require('lsp-zero')
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
+  },
 
-    lsp_zero.on_attach(function(client, bufnr)
-      lsp_zero.default_keymaps({buffer = bufnr})
-    end)
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_cmp()
 
-    require('mason').setup({})
-    require('mason-lspconfig').setup({
-      ensure_installed = { 'bashls', 'cssls', 'eslint', 'jsonls', 'lua_ls', 'tsserver', 'volar' }
-    })
+      local cmp = require('cmp')
+      local cmp_action = lsp_zero.cmp_action()
+      local lspkind = require('lspkind')
 
-    require('lspconfig').lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-          }
+      cmp.setup({
+        formatting = {
+          format = lspkind.cmp_format(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+        })
+      })
+    end
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'williamboman/mason-lspconfig.nvim',
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
+
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = bufnr})
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr})
+        vim.keymap.set({ "n", "v" }, '<leader>ca', vim.lsp.buf.code_action, {buffer = bufnr})
+      end)
+
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'bashls', 'cssls', 'eslint', 'jsonls', 'lua_ls', 'tsserver', 'volar' },
+        handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+
+          lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end
         }
-      }
-    })
-    lsp_zero.setup()
-
-    local cmp = require('cmp')
-    local lspkind = require('lspkind')
-    cmp.setup({
-      formatting = {
-        format = lspkind.cmp_format(),
-      },
-      mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-y>'] = cmp.mapping.confirm({select = true}),
-        ['<C-Space>'] = cmp.mapping.complete({}),
-        ['<C-e>'] = cmp.mapping.abort(),
-      }
-    })
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover)
-    vim.keymap.set({ "n", "v" }, '<leader>ca', vim.lsp.buf.code_action)
-  end
+      })
+    end
+  }
 }
