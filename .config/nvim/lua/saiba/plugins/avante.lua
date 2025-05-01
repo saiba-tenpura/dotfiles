e756a21fdfa1c74669c6aef95243ec00c08c61a1
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2025-05-01 10:22:31 +0200

    Add latest lockfile and some minor config adjustments.

--- .config/nvim/lua/saiba/plugins/avante.lua
+++ .config/nvim/lua/saiba/plugins/avante.lua
@@ -5,8 +5,6 @@ return {
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-      auto_suggestions_provider = "ollama",
-      debug = true,
       provider = "ollama",
       ollama = {
         endpoint = "http://127.0.0.1:11434",
--- .config/nvim/lua/saiba/plugins/colorscheme.lua
+++ .config/nvim/lua/saiba/plugins/colorscheme.lua
@@ -1,11 +1,10 @@
 return {
   {
     'saiba-tenpura/pywal-theme.nvim',
-    lazy = false,
     name = 'pywal-theme',
     dependencies = { 'rktjmp/lush.nvim' },
     config = function()
       vim.cmd.colorscheme('pywal-theme')
     end
-  }
+  },
 }
