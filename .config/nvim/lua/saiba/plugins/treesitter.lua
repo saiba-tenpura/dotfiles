c55d6e623c7a8bc12180db6f85d3ed9dd74d97f7
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2026-06-29 21:10:56 +0200

    Properly configure nvim-treesitter in Neovim configuration.

--- .config/nvim/lua/saiba/plugins/treesitter.lua
+++ .config/nvim/lua/saiba/plugins/treesitter.lua
@@ -4,5 +4,18 @@ return {
     branch = 'main',
     lazy = false,
     build = ':TSUpdate',
+    config = function()
+      local ensure_installed = { 'bash', 'html', 'javascript', 'json', 'lua', 'scss', 'typescript' }
+      require('nvim-treesitter').install(ensure_installed)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end
   },
 }
