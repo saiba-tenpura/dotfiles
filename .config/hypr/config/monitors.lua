b8744c65404c3875d666a27c8fd190ac4d7a403e
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2026-07-29 10:50:11 +0200

    Add Hyprland monitor configurations for desktop setup.

--- .config/hypr/config/monitors.lua
+++ .config/hypr/config/monitors.lua
@@ -1,3 +1,4 @@
+-- Laptop
 hl.monitor({
     output   = "",
     mode     = "preferred",
@@ -5,3 +6,26 @@ hl.monitor({
     scale    = "auto",
 })
 
+-- Desktop
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "1920x1080",
+    position = "0x0",
+    scale    = 1,
+})
+
+hl.monitor({
+    output   = "DP-1",
+    mode     = "3840x2160@160",
+    position = "auto",
+    scale    = 2,
+    vrr      = 1,
+})
+
+hl.monitor({
+    output   = "DP-2",
+    mode     = "3840x2160@144",
+    position = "auto",
+    scale    = 2,
+    vrr      = 1,
+})
